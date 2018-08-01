1d8a0de0bbc9e097ff868267aabc979f8790f7f5
SamuelPWeekes <33092469+SamuelPWeekes@users.noreply.github.com>
2018-08-01 14:02:43 +0100

    Add files via upload

---
+++ starter/partials/navbar.php
@@ -0,0 +1,12 @@
+    <nav class="navbar" role="navigation">
+      <a class="logo" href="#"><img src="public/assets/img/logo.svg" alt=""></a>
+      <ul>
+        <li class="nb-toggle">
+          <a class="overlay-trigger" data-overlay="nav-overlay" title="Open Site Navigation">
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+        </li>
+      </ul>
+    </nav>
