f047082f8e0b18adc576538165c40bee511ce7e6
Hamilton <hamiltonm@gmail.com>
2018-11-09 16:24:05 +0000

    fixing gitignore

--- src/main/java/brunel/mormur/Database/SessionDetails.java
+++ src/main/java/brunel/mormur/Database/SessionDetails.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.util.Date;
 import java.util.List;
 
 @Data
@@ -21,46 +20,28 @@ public class SessionDetails {
     @Column(name = "host")
     private Long hostId;
 
-
     @Column(name = "title")
     private String title;
 
-    @Column(name = "startTime")
-    private Date startTime;
-
-    @Column(name = "endTime")
-    private Date endTime;
-
-    @Column(name = "topic")
-    private String topic;
-
-
-    @OneToMany(targetEntity = QAResponse.class)
-    List<QAResponse> replies;
+    @OneToMany(targetEntity = Question.class)
+    List<Question> replies;
 
     @Version @JsonIgnore
-    private long version = 1;
+    private Long version;
+
+
+    public SessionDetails() {
+
+    }
+
     public SessionDetails(String title) {
         this.title = title;
         this.version = 1L;
     }
 
     public SessionDetails(String title, Long hostId, Long version) {
-    public Event(long hostId, String hostName, String title, Date startTime, Date endTime, String topic, List<QAResponse> replies) {
-        this.hostId = hostId;
-        this.hostName = hostName;
         this.title = title;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.topic = topic;
-        this.replies = replies;
-    }
-    public Event(long hostId, String hostName, String title, Date startTime, Date endTime, String topic) {
         this.hostId = hostId;
-        this.hostName = hostName;
-        this.title = title;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.topic = topic;
+        this.version = version;
     }
 }
